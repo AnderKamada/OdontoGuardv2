713e9363ce7c6edba3a37732531532c684a3853f
ander kamada <anderkamada3@gmail.com>
2025-03-25 20:10:51 -0300

    arrumando erro no api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://67e2dada97fc65f53537e22e.mockapi.io/tasks',
+  baseURL: 'https://67e2dada97fc65f53537e22e.mockapi.io',
 });
 
 export default api;
